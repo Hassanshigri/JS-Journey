54d593b17ab84a162d45b2af56c33d2f59681f86
Muhammad Hassan <hassanshigri435@gmail.com>
2024-03-02 12:21:36 +0500

    fetch Post method

--- fetchApi.js
+++ fetchApi.js
@@ -1,35 +1,35 @@
-fetch('https://reqres.in/api/users')
-.then(res => {
-    if (res.ok){
-        console.log('Success')
-    }else{
-        console.log('Fail')
-    }
-})
-.then(data => console.log(data))
-.catch(error => console.log('ERROR'))
-
-
-// fetch('https://reqres.in/api/users',{
-//     method: 'POST',
-//     //when you post in api you should tell fetch passing JSON 
-//     headers:{
-//         'Contant-Type':'application/json'
-//     },
-//     //convets JSON object into string
-//     body:JSON.stringify(
-//         {
-//             name: 'User 1'
-//         }
-//     )
-// })
-
-// .then(res=>{
+// fetch('https://reqres.in/api/users')
+// .then(res => {
 //     if (res.ok){
 //         console.log('Success')
 //     }else{
-//         console.log('Not successfull')
+//         console.log('Fail')
 //     }
 // })
-// .then(data=>console.log(data))
-// .catch(error=>console.log('ERROR'))
+// .then(data => console.log(data))
+// .catch(error => console.log('ERROR'))
+
+
+fetch('https://reqres.in/api/users',{
+    method: 'POST',
+    //when you post in api you should tell fetch passing JSON 
+    headers:{
+        'Contant-Type':'application/json'
+    },
+    //convets JSON object into string
+    body:JSON.stringify(
+        {
+            name: 'User 1'
+        }
+    )
+})
+
+.then(res=>{
+    if (res.ok){
+        console.log('Success')
+    }else{
+        console.log('Not successfull')
+    }
+})
+.then(data=>console.log(data))
+.catch(error=>console.log('ERROR'))
